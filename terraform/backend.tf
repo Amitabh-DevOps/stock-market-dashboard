e8cfa843e13b60d737f014988c8af6eb0a2db280
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-11-21 09:17:27 +0530

    restored previous bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "stock-dashboard-terraform-state1"
+    bucket         = "stock-dashboard-terraform-state"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
